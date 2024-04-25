8a02887561fa8f5860c9a398b287052a40385435
201250086 <1021224661@qq.com>
2024-04-25 18:59:09 +0800

    Add:增加了RosView

---
+++ src/components/RosList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>This is ros-list page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RosList"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/RosView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="roslist">
+    <img alt="Vue logo" src="../assets/bg_x.png">
+    <rosList msg="Welcome To Ros-view!"/>
+  </div>
+</template>
+
+<script>
+import rosList from "@/components/RosList";
+
+export default {
+  name: "RosView",
+  components: {
+    rosList
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
